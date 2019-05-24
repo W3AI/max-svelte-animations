97c2471b152fa28e0485a262f9ec89ca0e14fdd8
stefian <v_ianta@yahoo.com>
2019-05-24 07:56:27 -0400

    animations with intro and outro logging

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,12 @@
   import { writable } from "svelte/store";
   import { tweened } from "svelte/motion";
   import { cubicIn } from "svelte/easing";
-  import { fade, fly, slide, scale } from 'svelte/transition'; 
+  import { fade, fly, slide, scale } from "svelte/transition";
 
-  import Spring from './Spring.svelte'; 
+  import Spring from "./Spring.svelte";
 
   let boxInput;
+  let showParagraph = false;
 
   // default duration = 400ms
   const progress = tweened(0, {
@@ -22,34 +23,56 @@
   let boxes = [];
 
   function addBox() {
-      boxes = [...boxes, boxInput.value];
+    boxes = [...boxes, boxInput.value];
   }
 
   function discard(value) {
-      boxes = boxes.filter(el => el !== value);
+    boxes = boxes.filter(el => el !== value);
   }
 </script>
 
 <style>
-    div {
-        width: 1rem;
-        height: 1rem;
-        background: #ccc;
-        margin: 1rem;
-        box-shadow: 0 2px 8px rgba(0,0,0,0.26);
-        border-radius: 5px;
-        padding: 1rem;
-    }
+  div {
+    width: 1rem;
+    height: 1rem;
+    background: #ccc;
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    padding: 1rem;
+  }
 </style>
 
 <!-- <progress value={$progress} /> -->
 <!-- <Spring /> -->
 
-<input type="text" bind:this={boxInput}>
-<button on:click={addBox} >Add</button>
+<button
+  on:click={() => {
+    showParagraph = !showParagraph;
+  }}>
+  Toggle
+</button>
 
-{#each boxes as box (box)}
-    <div transition:fly={{duration: 400, easing: cubicIn, y:-300}}
-    on:click={discard.bind(this, box)} >{box}</div>
-{/each}
+{#if showParagraph}
+  <p transition:fly={{ y: 300 }}>Can you see me?</p>
+{/if}
 
+<hr />
+
+<input type="text" bind:this={boxInput} />
+<button on:click={addBox}>Add</button>
+
+{#if showParagraph}
+  {#each boxes as box (box)}
+    <div
+      transition:fly|local={{ duration: 400, easing: cubicIn, y: -300 }}
+      on:click={discard.bind(this, box)}
+      on:introstart={() => console.log('Adding the element starts')}
+      on:introend={() => console.log('Adding the element ends')}
+      on:outrostart={() => console.log('Removing the element starts')}
+      on:outroend={() => console.log('Removing the element ends')}
+      >
+      {box}
+    </div>
+  {/each}
+{/if}
