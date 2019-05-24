46df3b9ba8e7787e9c3a0c259fc1114223c0f26c
stefian <v_ianta@yahoo.com>
2019-05-23 22:19:25 -0400

    added a tweened store and linked it to a progress bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script>
+    import { writable } from 'svelte/store';
+    import { tweened } from 'svelte/motion';
+
+    const progress = tweened(0);
+
+    setTimeout(() => {
+        progress.set(.5);
+    }, 1500);
 </script>
+
+<progress value={$progress}></progress>
 
 
 
